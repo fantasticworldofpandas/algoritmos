e59bdce8a30ccbd51ab396168b077ad8a9ab8974
atevilson <freitas.atevilson@gmail.com>
2024-03-15 21:19:19 -0300

    remove lista, add função range

--- algoritmos/algoritmo_tabuada.py
+++ algoritmos/algoritmo_tabuada.py
@@ -1,8 +1,8 @@
-# solicita um numero para o usuario e retorna sua tabuada de 1 a 10
+# esse algoritmo solicita um numero para o usuario e retorna sua tabuada de 1 a 10
 n = int(input('Insira um numero: '))
 print(f'\nA tabuada do n° ({n})')
 
-lista_tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_tabuada = range(1,11)
 
 for x in lista_tabuada:
     resultado = x * n
